74b5fa6142608a39fc82fb1bed5d22919e7d1ba7
Muhammad Rafiuddin <71545866+rafimrfdn@users.noreply.github.com>
2024-04-05 16:07:05 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,6 +18,172 @@ app.get('/api/increment', (c) => {
   return c.html(updatedHTML);
 });
 
+
+// Make sure to use json() middleware to parse JSON request bodies
+
+app.post('/submit', async (c) => {
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbyNTqOTlSBJ1u6Q4vrSsEcw4LKJTOexV1I4eXxdhoE0DcrbEVMrsMQPurkYiFHoJtrWpw/exec';
+const TABLE = 'Users';
+
+  try {
+    const lastId = await getLastId(BASE_URL, TABLE);
+    const newId = lastId + 1;
+
+    const requestBody = await c.req.parseBody();
+    const data = {
+      id: newId,
+      username: requestBody.username,
+      email: requestBody.email
+      // Additional data fields as needed
+    };
+
+    const response = await fetch(`${BASE_URL}?action=insert&table=${TABLE}&data=${encodeURIComponent(JSON.stringify(data))}`, {
+      method: 'GET', // Or 'POST', adjust according to your API requirements
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return c.html(`Data dengan <b>id</b> ${data.id} <b>${data.username}</b> berhasil diterima`);
+  } catch (error) {
+    console.error('Error submitting data:', error);
+    return c.status(500).json({ error: 'An error occurred while submitting data.' });
+  }
+});
+
+
+
+const READLAST = 'https://script.google.com/macros/s/AKfycbyNTqOTlSBJ1u6Q4vrSsEcw4LKJTOexV1I4eXxdhoE0DcrbEVMrsMQPurkYiFHoJtrWpw/exec?action=read&table=Users';
+async function getLastId(READLAST) {
+  const response = await fetch(READLAST);
+  if (!response.ok) {
+    throw new Error('Failed to fetch the last ID');
+  }
+  const data = await response.json();
+  const lastItem = data.data[data.length - 1];
+  return lastItem ? lastItem.id : 0;
+}
+
+
+
+  
+
+
+
+
+app.get('/users', async (c) => {
+  const url = 'https://script.google.com/macros/s/AKfycbyNTqOTlSBJ1u6Q4vrSsEcw4LKJTOexV1I4eXxdhoE0DcrbEVMrsMQPurkYiFHoJtrWpw/exec?action=read&table=Users';
+
+  const startTime = performance.now(); // Record the start time
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    const datas = data.data;
+    const processingTime = performance.now() - startTime;
+
+    return c.html(`
+      <html>
+      <head>
+          <title>Appscript data</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://cdn.simplecss.org/simple.css">
+      </head>
+      <body>
+      <nav>
+      <a href="/">home</a>
+      <a href="/products">products</a>
+      <a href="/users">users</a>
+      </nav>
+      <h2>Appscript data</h2>
+                  <p>Terdapat ${datas.length} data. Data berhasil diproses dalam waktu: <strong>${processingTime} milliseconds</strong></p>
+      <table>
+      <tr>
+      <th>id</th>
+      <th>action</th>
+      </tr>
+             ${datas.map(data => `
+      <tr>
+              <td>${data.id}</td>
+              <td><a href="/user/${data.id}">Detail</a></td>
+      </tr>
+      `).join('')}
+      </table>
+  
+      </body>
+      </html>
+      `);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return c.status(500).text('Error fetching data');
+  }
+});
+
+
+
+
+
+app.get('/user/:id', async (c) => {
+
+  const URL = 'https://script.google.com/macros/s/AKfycbyNTqOTlSBJ1u6Q4vrSsEcw4LKJTOexV1I4eXxdhoE0DcrbEVMrsMQPurkYiFHoJtrWpw/exec?action=read&table=Users&id=';
+  const startTime = performance.now(); // Record the start time
+
+  const id = c.req.param('id');
+
+  try {
+    const response = await fetch(URL + id );
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json();
+    const datas = data.data;
+    const processingTime = performance.now() - startTime;
+
+    return c.html(`
+      <html>
+      <head>
+          <title>${datas.username} detail</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://cdn.simplecss.org/simple.css">
+      </head>
+      <body>
+      <nav>
+      <a href="/">home</a>
+      <a href="/products">products</a>
+      <a href="/users">users</a>
+      </nav>
+      <h2>${datas.username} detail</h2>
+      <p>Data berhasil diproses dalam waktu: <strong>${processingTime} milliseconds</strong></p>
+      <table>
+      <tr>
+      <th>id</th>
+      <th>email</th>
+      <th>username</th>
+      </tr>
+      <tr>
+      <td>${datas.id}</td>
+      <td>${datas.email}</td>
+      <td>${datas.username}</td>
+      </tr>
+      </table>
+      </body>
+      </html>
+    `);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return c.status(500).text('Error fetching data');
+  }
+});
+
+
+
+
+
 app.get('*', serveStatic({ path: './static/fallback.txt' }))
 
 Deno.serve(app.fetch)
